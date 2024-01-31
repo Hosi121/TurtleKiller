ace0ab9403b26f894933d249c22b9b322fc3d99e
Takuya Kataiwa <takuyakataiwa@icloud.com>
2024-01-31 14:02:55 +0900

    refactor: finished changes2

--- src/main/java/org/lwjglb/engine/IInitializer.java
+++ src/main/java/org/lwjglb/engine/IInitializer.java
@@ -1,13 +1,10 @@
 package org.lwjglb.engine;
 
-import org.lwjglb.engine.*;
-import org.lwjglb.engine.graph.*;
 import org.lwjglb.engine.scene.*;
 
 public interface IInitializer {
     void initTerrain(Scene scene);
     void initMap(Scene scene);
-    void initCubes(Scene scene);
     void initLighting(Scene scene);
     void initSkybox(Scene scene);
     void initCamera(Scene scene, Window window);
--- src/main/java/org/lwjglb/engine/scene/GameMap.java
+++ src/main/java/org/lwjglb/engine/scene/GameMap.java
@@ -1,2 +1,74 @@
-package org.lwjglb.engine.scene;public class GameMap {
+package org.lwjglb.engine.scene;
+
+import org.lwjglb.engine.Utils;
+import org.lwjglb.engine.graph.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameMap {
+    private List<Entity> entities;
+    private List<List<List<Float>>> position;
+
+    public GameMap(String filePath, Scene scene) {
+        this.entities = new ArrayList<>();
+        this.position = new ArrayList<>();
+        try{
+            String jsonContent = Utils.readFile(filePath);
+            setMap(jsonContent, scene);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setMap(String jsonContent, Scene scene) throws JSONException {
+        JSONArray jsonArray = new JSONArray(jsonContent);
+
+        Model cubeModel = ModelLoader.loadModel("cube-model", "resources/models/cube/cube.obj",
+                scene.getTextureCache(), scene.getMaterialCache(), false);
+        scene.addModel(cubeModel);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String cubeID = jsonObject.getString("id");
+            JSONArray posArray = jsonObject.getJSONArray("position");
+            float x = (float) posArray.getDouble(0);
+            float y = (float) posArray.getDouble(1);
+            float z = (float) posArray.getDouble(2);
+
+            Entity cubeEntity = new Entity(cubeID, cubeModel.getId());
+            cubeEntity.setPosition(x, y, z);
+            cubeEntity.updateModelMatrix();
+
+            entities.add(cubeEntity);
+            position.add(setPositionList(x, y, z));
+        }
+    }
+
+    private List<List<Float>> setPositionList(float x, float y, float z) {
+        List<List<Float>> position = new ArrayList<>();
+        List<Float> xList = new ArrayList<>();
+        xList.add(x);
+        List<Float> yList = new ArrayList<>();
+        yList.add(y);
+        List<Float> zList = new ArrayList<>();
+        zList.add(z);
+
+        position.add(xList);
+        position.add(yList);
+        position.add(zList);
+
+        return position;
+    }
+
+    public List<Entity> getEntities() {
+        return entities;
+    }
+
+    public List<List<List<Float>>> getposition() {
+        return position;
+    }
 }
+
